f59a31f7a0dc98714c0efe17e451cd64f02dc8ca
Tong_style <1141632973@qq.com>
2016-08-08 00:20:59 +0800

    add a get API

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = new express();
 
+app.get('/', (req, res) => {
+  res.json(['text', 'date']);
+})
+
 app.listen(9999, () => {
   console.log('server is run');
 })
